5fcf4d5f804fd57ccfff6df037b20b91c8b55e82
wesleyfernandesprojetofaculdade <wesley@viaapia.com.br>
2026-09-02 14:37:56 -0300

    feat(venda): modelo e serviços básicos

--- frontend/stockonyou-frontend/src/app/core/model/venda.model.ts
+++ frontend/stockonyou-frontend/src/app/core/model/venda.model.ts
@@ -1,2 +1,28 @@
-export interface VendaModel {
+import { Produto } from "./produto.model";
+export interface ItemVendaRequest {
+  produtoId: number;
+  quantidade: number;
+  precoUnitario: number;
+}
+
+export interface ItemVendaResponse {
+  id: number;
+  produto: Produto;
+  quantidade: number;
+  precoUnitario: number;
+  subtotal: number;
+}
+
+export interface VendaRequest {
+  clienteNome?: string;
+  itens: ItemVendaRequest[];
+}
+
+export interface VendaResponse {
+  id: number;
+  dataVenda: string;
+  clienteNome?: string;
+  valorTotal: number;
+  itens: ItemVendaResponse[];
+  usuarioNome?: string;
 }
---
+++ frontend/stockonyou-frontend/src/app/core/services/venda.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PageResponse } from '../model/produto.model';
+import { VendaRequest, VendaResponse } from '../model/venda.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VendaService {
+  private readonly http = inject(HttpClient);
+  private readonly apiUrl = 'http://localhost:8080/api/vendas';
+
+  listarComFiltros(
+    clienteNome?: string,
+    dataInicio?: string,
+    dataFim?: string,
+    page: number = 0,
+    size: number = 10
+  ) : Observable<PageResponse<VendaResponse>> {
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString())
+      .set('sort', 'id,desc')
+    if (clienteNome) params = params.set('clienteNome', clienteNome);
+    if (dataInicio) params = params.set('dataInicio', dataInicio);
+    if (dataFim) params = params.set('dataFim', dataFim);
+    return this.http.get<PageResponse<VendaResponse>>(this.apiUrl, { params })
+  }
+
+  buscarPorId(id: number): Observable<VendaResponse> {
+    return this.http.get<VendaResponse>(`${this.apiUrl}/${id}`);
+  }
+
+  realizarVenda(venda: VendaRequest): Observable<VendaResponse> {
+    return this.http.post<VendaResponse>(this.apiUrl, venda);
+  }
+}
